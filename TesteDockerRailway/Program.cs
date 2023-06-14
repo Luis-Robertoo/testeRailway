da35c567820f64fea6f7aaa48de2aad4d80b887e
Luis Roberto <luis.r.s14@hotmail.com>
2023-06-13 22:59:20 -0300

    adicionado variaveis

--- TesteDockerRailway/Program.cs
+++ TesteDockerRailway/Program.cs
@@ -2,10 +2,19 @@
 
 
 Console.WriteLine("Hello, World!");
+
 var counter = 0;
+var variavel = Environment.GetEnvironmentVariable("PORTA");
+var parar = Environment.GetEnvironmentVariable("PARAR");
+
 var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
-while (max is -1 || counter < max)
+
+bool parou = parar is null ? false : parar.Equals("true") ? true : false;
+
+while ((max is -1 || counter < max) && !parou)
 {
+    Console.WriteLine(variavel);
+
     Console.WriteLine($"Counter: {++counter}");
     await Task.Delay(TimeSpan.FromMilliseconds(1_000));
 }
