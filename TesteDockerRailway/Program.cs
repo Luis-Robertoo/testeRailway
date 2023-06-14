76415d516e30b6e138435026bd5e0c3d6034b174
Luis Roberto <luis.r.s14@hotmail.com>
2023-06-13 23:12:28 -0300

    colocado limitador padrao

--- TesteDockerRailway/Program.cs
+++ TesteDockerRailway/Program.cs
@@ -5,13 +5,16 @@ Console.WriteLine("Hello, World!");
 
 var counter = 0;
 var variavel = Environment.GetEnvironmentVariable("PORTA");
+
+variavel = variavel is null ? "100" : variavel.ToString();
+
 var parar = Environment.GetEnvironmentVariable("PARAR");
 
 var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
 
 bool parou = parar is null ? false : parar.Equals("true") ? true : false;
 
-while ((max is -1 || counter < max) && !parou)
+while ((max is -1 || counter < max) && !parou && (variavel != null && Convert.ToInt32(variavel) > counter))
 {
     Console.WriteLine(variavel);
 
