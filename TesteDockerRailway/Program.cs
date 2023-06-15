d70f3e2336f85e480e5ccb8c394b653cf0464e86
Luis Roberto <luis.r.s14@hotmail.com>
2023-06-14 23:03:18 -0300

    sera

--- TesteDockerRailway/Program.cs
+++ TesteDockerRailway/Program.cs
@@ -1,10 +1,10 @@
 ﻿
 using System.Collections;
+using static System.Net.WebRequestMethods;
+
 Console.WriteLine("-=-=-=-=-=-=-=--=-==--=-=-==--=-==-=--=-=-=-=-=--=-=-=-=----=-=-=");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("-=-=-=-=-=-=-=--=-==--=-=-==--=-==-=--=-=-=-=-=--=-=-=-=----=-=-=");
-
-
 
 var variaveis = Environment.GetEnvironmentVariables();
 
@@ -16,6 +16,10 @@ foreach (DictionaryEntry variavel in variaveis)
 }
 
 Console.WriteLine("-=-=-=-=-=-=-=--=-==--=-=-==--=-==-=--=-=-=-=-=--=-=-=-=----=-=-=");
+
+var porta = Environment.GetEnvironmentVariable("PORT");
+
+Environment.SetEnvironmentVariable("ASPNETCORE_URLS", $"http://+:{porta}");
 
 var builder = WebApplication.CreateBuilder(args);
 
